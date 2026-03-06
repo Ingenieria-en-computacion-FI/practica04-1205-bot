684db8d8da8009f0e1f521eca1386d30c34a7202
unknown <ypollo20@gmail.com>
2026-03-05 21:45:28 -0600

    Subo malloc.c

--- examples/malloc.c
+++ examples/malloc.c
@@ -13,7 +13,7 @@ int main (){
   if (array!=NULL) {
     printf("[ ");
     for (int i = 0; i < n; i++) {
-      printf("%d ", (array + i));
+      printf("%d ", *(array + i));
     }
     printf("]\n");
     free(array);
